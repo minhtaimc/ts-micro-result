3424cc8b19101ff9791ce900b50b0bc54d7c0bea
minhtaimc <minhtaimc@gmail.com>
2025-10-17 14:24:00 +0700

    2.0.2

--- src/index.ts
+++ src/index.ts
@@ -107,7 +107,6 @@ export function validationErrors(
 export function inferStatus<T>(result: Result<T>): number {
   if (result.status) return result.status
   if (result.isOk()) {
-    // Nếu không có data thì trả về 204 No Content
     if (result.data === null || result.data === undefined) return 204
     return 200
   }
